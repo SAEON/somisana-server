12a56cc75d461081f2b37b52ae7b96812e58d61f
dylan <d.pivo@saeon.nrf.ac.za>
2025-06-09 13:47:00 +0200

    Added db tests

--- test/factories.py
+++ test/factories.py
@@ -7,7 +7,7 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 
 import somisana.db
 from somisana.const import ResourceType, ResourceReferenceType
-from somisana.db.models import Product, Simulation, Resource
+from somisana.db.models import Product, ProductVersion, ProductResource, Dataset, DatasetResource, Resource
 
 FactorySession = scoped_session(sessionmaker(
     bind=somisana.db.engine,
@@ -37,24 +37,30 @@ class ProductFactory(SOMISANAModelFactory):
     north_bound = factory.LazyAttribute(lambda o: random.uniform(o.south_bound, 0))
     west_bound = factory.Faker('pyfloat', left_digits=3, right_digits=6, min_value=-180, max_value=170)
     east_bound = factory.LazyAttribute(lambda o: random.uniform(o.west_bound, 180))
-
-    @factory.post_generation
-    def simulations(obj, create, simulations):
-        if simulations:
-            for simulation in simulations:
-                obj.simulations.append(simulation)
-            if create:
-                FactorySession.commit()
+    horizontal_resolution = factory.Faker('word')
+    vertical_extent = factory.Faker('word')
+    vertical_resolution = factory.Faker('word')
+    temporal_extent = factory.Faker('word')
+    temporal_resolution = factory.Faker('word')
+    variables = factory.Faker('word')
 
 
-class SimulationFactory(SOMISANAModelFactory):
+class ProductVersionFactory(SOMISANAModelFactory):
     class Meta:
-        model = Simulation
+        model = ProductVersion
+
+    product = factory.SubFactory(ProductFactory)
+    superseded_product = factory.SubFactory(ProductFactory)
+
+
+class DatasetFactory(SOMISANAModelFactory):
+    class Meta:
+        model = Dataset
 
     id = factory.Sequence(lambda n: n)
-    title = factory.Faker('catch_phrase')
-    folder_path = factory.Faker('file_path', depth=3)
-    data_access_url = factory.Faker('uri')
+    product = factory.SubFactory(ProductFactory)
+    title = factory.Faker('sentence', nb_words=3)
+    folder_path = factory.Faker('file_path', depth=2, extension='nc')
 
 
 class ResourceFactory(SOMISANAModelFactory):
@@ -62,9 +68,31 @@ class ResourceFactory(SOMISANAModelFactory):
         model = Resource
 
     id = factory.Sequence(lambda n: n)
-    product_id = factory.SelfAttribute('product.id')
+    title = factory.Faker('sentence', nb_words=3)
     reference = factory.Faker('uri')
     reference_type = factory.LazyAttribute(lambda _: random.choice(list(ResourceReferenceType)).value)
     resource_type = factory.LazyAttribute(lambda _: random.choice(list(ResourceType)).value)
 
+
+class ProductResourceFactory(SOMISANAModelFactory):
+    class Meta:
+        model = ProductResource
+
+    product_id = factory.SelfAttribute('product.id')
+    resource_id = factory.SelfAttribute('resource.id')
+
     product = factory.SubFactory(ProductFactory)
+    resource = factory.SubFactory(ResourceFactory)
+
+
+class DatasetResourceFactory(SOMISANAModelFactory):
+    class Meta:
+        model = DatasetResource
+
+    dataset_id = factory.SelfAttribute('dataset.id')
+    resource_id = factory.SelfAttribute('resource.id')
+
+    dataset = factory.SubFactory(DatasetFactory)
+    resource = factory.SubFactory(ResourceFactory)
+
+
--- test/test_db.py
+++ test/test_db.py
@@ -1,51 +1,41 @@
-from .factories import (
-    ProductFactory,
-    SimulationFactory, ResourceFactory
-)
 from sqlalchemy import select
 
 from somisana.db.models import (
     Product,
-    Simulation,
     Resource
 )
 from test import TestSession
+from .factories import (
+    ProductFactory, ProductResourceFactory,
+    DatasetFactory, ResourceFactory, DatasetResourceFactory,
+    ProductVersionFactory
+)
 
 
 def test_product():
+    old_product = ProductFactory()
     product = ProductFactory()
-    result = TestSession.execute(select(Product)).scalar()
+
+    ProductVersionFactory(product=product, superseded_product=old_product)
+
+    resource_1 = ResourceFactory()
+    resource_2 = ResourceFactory()
+
+    ProductResourceFactory(product=product, resource=resource_1)
+    ProductResourceFactory(product=product, resource=resource_2)
+
+    dataset_1 = DatasetFactory(product=product)
+    dataset_2 = DatasetFactory(product=product)
+
+    resource_3 = ResourceFactory()
+    resource_4 = ResourceFactory()
+
+    DatasetResourceFactory(dataset=dataset_1, resource=resource_3)
+    DatasetResourceFactory(dataset=dataset_2, resource=resource_4)
+
+    result = TestSession.get(Product, product.id)
 
     compare_products(product, result)
-
-
-def test_simulation():
-    simulation = SimulationFactory()
-    result = TestSession.execute(select(Simulation)).scalar()
-
-    assert (
-               result.id,
-               result.folder_path,
-               result.data_access_url,
-           ) == (
-               simulation.id,
-               simulation.folder_path,
-               simulation.data_access_url,
-           )
-
-
-def test_product_with_simulations():
-    simulation1 = SimulationFactory()
-    simulation2 = SimulationFactory()
-
-    product = ProductFactory(simulations=[simulation1, simulation2])
-
-    result = TestSession.execute(select(Product)).scalar()
-
-    compare_products(product, result)
-
-    for index, simulation in enumerate(result.simulations):
-        compare_simulations(simulation, [simulation1, simulation2][index])
 
 
 def test_resource():
@@ -79,7 +69,13 @@ def compare_products(product1, product2):
                product1.north_bound,
                product1.south_bound,
                product1.east_bound,
-               product1.west_bound
+               product1.west_bound,
+               product1.horizontal_resolution,
+               product1.vertical_extent,
+               product1.vertical_resolution,
+               product1.temporal_extent,
+               product1.temporal_resolution,
+               product1.variables
            ) == (
                product2.id,
                product2.title,
@@ -88,33 +84,56 @@ def compare_products(product1, product2):
                product2.north_bound,
                product2.south_bound,
                product2.east_bound,
-               product2.west_bound
+               product2.west_bound,
+               product2.horizontal_resolution,
+               product2.vertical_extent,
+               product2.vertical_resolution,
+               product2.temporal_extent,
+               product2.temporal_resolution,
+               product2.variables
            )
 
+    for dataset in product1.datasets:
+        matching_dataset = next((d for d in product2.datasets if d.id == dataset.id), None)
+        assert matching_dataset is not None
+        compare_datasets(dataset, matching_dataset)
 
-def compare_simulations(simulation1, simulation2):
+    for resource in product1.resources:
+        matching_resource = next((r for r in product2.resources if r.id == resource.id), None)
+        assert matching_resource is not None
+        compare_resources(resource, matching_resource)
+
+
+def compare_datasets(dataset1, dataset2):
     assert (
-               simulation1.id,
-               simulation1.folder_path,
-               simulation1.data_access_url,
+               dataset1.id,
+               dataset1.product_id,
+               dataset1.title,
+               dataset1.folder_path
            ) == (
-               simulation2.id,
-               simulation2.folder_path,
-               simulation2.data_access_url,
+               dataset2.id,
+               dataset2.product_id,
+               dataset2.title,
+               dataset2.folder_path
            )
+
+    for resource in dataset1.resources:
+        matching_resource = next((r for r in dataset2.resources if r.id == resource.id), None)
+        assert matching_resource is not None
+        compare_resources(resource, matching_resource)
 
 
 def compare_resources(resource1, resource2):
     assert (
-        resource1.id,
-        resource1.product_id,
-        resource1.reference,
-        resource1.reference_type,
-        resource1.resource_type,
-    ) == (
-        resource2.id,
-        resource2.product_id,
-        resource2.reference,
-        resource2.reference_type,
-        resource2.resource_type,
-    )
+               resource1.id,
+               resource1.title,
+               resource1.reference,
+               resource1.reference_type,
+               resource1.resource_type
+           ) == (
+               resource2.id,
+               resource2.title,
+               resource2.reference,
+               resource2.reference_type,
+               resource2.resource_type
+           )
